19b97f562ae624dbd134b2df714522f72ea3c20a
mariamaksimova <Maria.ms004@mail.ru>
2022-02-28 20:39:46 +0300

    tests

--- src/test/java/ModelTest.java
+++ src/test/java/ModelTest.java
@@ -4,25 +4,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ModelTest {
-    Humanoids artur = new Humanoids("Artur", 0, 0, 0,false, false);
-    Humanoids trillian = new Humanoids("Trillion", 0, 0, 0,false, false);
-    Mice mice = new Mice("Ben", 0, 0,0);
     Coordinates coordinates = new Coordinates(2, -2, 2);
-    Humanoids artur = new Humanoids("Artur", coordinates, true,false);
+    Coordinates coordinates1 = new Coordinates(0, -3, -5);
+    Humanoids artur = new Humanoids("Artur", coordinates,false,false);
+    Humanoids trillian = new Humanoids("Trillion", coordinates,false, false);
+    Humanoids ford = new Humanoids("Ford", coordinates1,false, false);
+
     Mice mice = new Mice("Ben", coordinates);
     Door door = new Door();
     CupsForMice cups = new CupsForMice(new Mice("Franky", coordinates));
 
-    @Test
-    public void captureTest(){
-        artur.capture();
-        assertTrue(artur.getIsCaptured());
-    }
+
 
     @Test
     public void captureExceptionTest(){
-        artur.capture();
-        Exception exception = assertThrows(Exception.class, () -> artur.capture());
+        trillian.capture(artur);
+        Exception exception = assertThrows(Exception.class, () -> trillian.capture(artur));
         assertEquals("Error capture", exception.getMessage());
     }
 
@@ -32,16 +29,16 @@ public class ModelTest {
         assertTrue(artur.getIsHypnotized());
     }
 
-    @Test
-    public void isOpenedTest(){
-        artur.openTheDoor(door);
-        assertTrue(door.getIsOpened());
-    }
+//    @Test
+//    public void isOpenedTest(){
+//        artur.openTheDoor(door, );
+//        assertTrue(door.getIsOpened());
+//    }
 
     @Test
     public void openDoorExceptionTest(){
-        artur.openTheDoor(door);
-        Exception exception = assertThrows(Exception.class, () -> artur.openTheDoor(door));
+        ford.openTheDoor(door,trillian);
+        Exception exception = assertThrows(Exception.class, () -> artur.openTheDoor(door,trillian));
         assertEquals("Door already open", exception.getMessage());
     }
 
